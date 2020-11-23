2bb5cd383cae389377e5fafa0e3d330ab6adcf51
y-taleb <yousseftaleb1999@gmail.com>
2020-11-23 15:05:05 +0100

    [Core] Added connect button.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "ODALID.h"
+#include <QDebug>
+#include <QtGui>
+#define PSZHOST "192.168.1.4"
 
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
@@ -13,3 +17,17 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+ReaderName MonLecteur;
+char pszHost[] = PSZHOST;
+
+
+void MainWindow::on_Connect_clicked()
+{
+    uint16_t status = 0;
+    //MonLecteur.Type = ReaderTCP;
+    //strcpy(MonLecteur.IPReader, pszHost);
+    MonLecteur.Type = ReaderCDC;
+    MonLecteur.device = 0;
+    status = OpenCOM(&MonLecteur);
+    qDebug() << "OpenCOM: '" << status << "'";
+}
